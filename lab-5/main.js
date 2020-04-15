63a6254e407923fb67d413fb5b19d50737000623
Sergeyshilo <56796942+Sergeyshilo@users.noreply.github.com>
2020-04-15 16:27:41 +0300

    Update main.js

--- lab-5/main.js
+++ lab-5/main.js
@@ -3,25 +3,43 @@ for (var i = 0; i < 9; i++){
     images[i] = new Image();
     images[i].src ='images/' + (i+1) + '.jpg';
 }
+var mainImg = document.getElementById('image');
 
-init();
+initialize();
 
 function randomImage(images) {
   var random = randomize(images);
-  while(randomItem === document.getElementById("image").src){
+  
+  while(randomItem === mainImg.src){
     random = randomize(images)
   }
   var randomItem = images[random].src;
-  document.getElementById("image").src = randomItem.toString();
+  mainImg.src = randomItem.toString();
+ 
 }
 
 function randomize(array){
   return Math.floor((Math.random() * (array.length)));
 }
 
-function init() {
+function initialize() {
   document.getElementById("switch-image").addEventListener("click", function(){
     randomImage(images);
   });
   randomImage(images);
+}
+
+function imageSize() {
+    var widthImage = document.getElementById("width").value;
+    var heightImage = document.getElementById("height").value;
+    var borderWidth = document.getElementById("border-width").value;
+    var descriptionValue = document.getElementById("description-value").value;
+    mainImg.style.width = widthImage+'px';
+    mainImg.style.height = heightImage+'px';
+    mainImg.style.borderWidth = borderWidth+'px';
+    document.getElementById("description").innerHTML = descriptionValue;
+}
+document.getElementById("size").addEventListener("click", function(){
+    imageSize();
+});
 }
