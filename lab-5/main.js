6c85124660270aa405562376d2d57653653fed78
Sergeyshilo <56796942+Sergeyshilo@users.noreply.github.com>
2020-04-15 13:14:01 +0300

    Update main.js

--- lab-5/main.js
+++ lab-5/main.js
@@ -1,12 +1,12 @@
 var images = new Array(9);
-for (var i = 1; i < 10; i++){
+for (var i = 0; i < 9; i++){
     images[i] = new Image();
-    images[i].src ='images/' + i + '.jpg';
+    images[i].src ='images/' + (i+1) + '.jpg';
 }
 
 init();
 
-function random_image(images) {
+function randomImage(images) {
   var random = randomize(images);
   var randomItem = images[random].src;
   while(randomItem === document.getElementById("image").src){
@@ -21,7 +21,7 @@ function randomize(array){
 
 function init() {
   document.getElementById("switch-image").addEventListener("click", function(){
-    random_image(images);
+    randomImage(images);
   });
-  random_image(images);
+  randomImage(images);
 }
